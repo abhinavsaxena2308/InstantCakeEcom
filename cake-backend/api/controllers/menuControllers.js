b282896d1b638ec6a7bb07616bf7771438bd7541
abhinavsaxena2308 <abhinav.saxena.2003@gmail.com>
2025-09-23 01:19:14 +0530

    rename

--- cake-backend/api/controllers/menuControllers.js
+++ cake-backend/api/controllers/menuControllers.js
@@ -1,4 +1,4 @@
-const Menu = require("../models/Menu");
+const Menu = require("../models/menu");
 
 const getAllMenuItems = async(req, res) => {
     try {
--- cake-backend/api/routes/menuRoutes.js
+++ cake-backend/api/routes/menuRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Menu = require("../models/Menu");
+const Menu = require("../models/menu");
 const router = express.Router();
 
 const menuController = require('../controllers/menuControllers')
